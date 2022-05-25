9d2112d2a72eae83b1303f9cdb1d7947e46c6735
Aiswarya Sarath <49971693+aishwaryasarath@users.noreply.github.com>
2022-05-25 18:05:47 -0500

    Create main.tf

---
+++ 03-ec2/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.15.1"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+  
+}
+resource "aws_instance" "ec2" {
+
+    ami = "ami-0022f774911c1d690"
+    instance_type = "t2.micro"
+  
+}
