f6ad0c8ccc53fe052d472b68bf7aad9b7196074b
Aiswarya Sarath <49971693+aishwaryasarath@users.noreply.github.com>
2022-05-25 18:13:06 -0500

    Update main.tf

--- 04-eip/main.tf
+++ 04-eip/main.tf
@@ -1,1 +1,30 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.15.1"
+    }
+  }
+}
+
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "ec2" {
+
+    ami = "ami-0022f774911c1d690"
+    instance_type = "t2.micro"
+  
+}
+
+resource "aws_eip" "elasticeip" {
+    instance = aws_instance.ec2.id
+  
+}
+
+output "EIP" {
+    value = aws_eip.elasticeip.public_ip
+}
