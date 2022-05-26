d68b2672530e06207d8763f6542f3d93147382fe
Aiswarya Sarath <49971693+aishwaryasarath@users.noreply.github.com>
2022-05-26 11:00:06 -0500

    Update main.tf

--- 08-module-outputs/main.tf
+++ 08-module-outputs/main.tf
@@ -1,1 +1,14 @@
 
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+module "ec2module" {
+    source = "./ec2-module"
+    ec2name = "Name from module"
+}
+
+output "module_output" {
+    value = module.ec2module.instance_id
+}
